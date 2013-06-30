e54a99d93e652aa28274e717cb948fa6abfa03fe
KAWACHI Takashi <kawachi@p-lucky.net>
2013-06-30 17:24:16 +0900

    Use hipchat lib

--- hipbot.go
+++ hipbot.go
@@ -1,15 +1,45 @@
 package main
 
 import (
+	"flag"
+	"github.com/tkawachi/hipchat"
+	"log"
 	"os"
-	"fmt"
+	"strings"
 )
 
-func configFile() string {
-	return os.Getenv("HOME") + "/.hipbot"
-}
+var configFile = flag.String("config", os.Getenv("HOME")+"/.hipbot", "config file")
 
-func main () {
-	config := loadConfig(configFile())
-	fmt.Println(config, len(config.Hipchat.Rooms))
+const (
+	ConfDomain = "conf.hipchat.com"
+)
+
+func main() {
+	flag.Parse()
+	config := loadConfig(*configFile)
+	chatConfig := config.Hipchat
+	log.Println(chatConfig)
+
+	client, err := hipchat.NewClient(
+		chatConfig.Username, chatConfig.Password, chatConfig.Resource)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, room := range chatConfig.Rooms {
+		if !strings.Contains(room, "@") {
+			room = room + "@" + ConfDomain
+		}
+		client.Join(room, chatConfig.Nickname)
+	}
+	go client.KeepAlive()
+	msgs := client.Messages()
+	for {
+		msg := <-msgs
+		if msg.Delay == nil {
+			if strings.HasPrefix(msg.Body, "wikipedia ") {
+				log.Println("wikipedia, from:", msg.From)
+				client.Say(msg.From, chatConfig.Nickname, "Searching wikipedia ...")
+			}
+		}
+	}
 }
