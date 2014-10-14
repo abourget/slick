252344d258c48ef1bb3887f961117592bd2ac7f3
Alexandre Bourget <alex@bourget.cc>
2014-10-14 10:23:28 -0400

    Added some coverage to the new Conversation.

--- conversation_test.go
+++ conversation_test.go
@@ -3,6 +3,8 @@ package plotbot
 import (
 	"testing"
 	"time"
+
+	"github.com/tkawachi/hipchat"
 )
 
 func TestConversationCheckParams(t *testing.T) {
@@ -17,4 +19,42 @@ func TestConversationCheckParams(t *testing.T) {
 }
 
 func TestDefeaultFilter(t *testing.T) {
+	c := &Conversation{}
+	u := &User{JID: "a_user"}
+	m := &Message{Message: &hipchat.Message{Body: "hello mama"}, FromUser: u}
+
+	if defaultFilterFunc(c, m) != true {
+		t.Error("defaultFilterFunc Failed")
+	}
+
+	type El struct {
+		c *Conversation
+		r bool
+	}
+	tests := []El{
+		El{&Conversation{}, true},
+
+		El{&Conversation{
+			Contains: "moo",
+		}, false},
+
+		El{&Conversation{
+			Contains: "MAMA",
+		}, true},
+
+		El{&Conversation{
+			WithUser: u,
+		}, true},
+
+		El{&Conversation{
+			WithUser: &User{JID: "another_user"},
+		}, false},
+
+	}
+
+	for i, el := range tests {
+		if defaultFilterFunc(el.c, m) != el.r {
+			t.Error("defaultFilterFunc Failed, index ", i)
+		}
+	}
 }
