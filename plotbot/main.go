82d23f21ac91cd2b4cb40183397f737a5556d44c
bpostlethwaite <post.ben.here@gmail.com>
2014-11-22 16:29:10 -0500

    reorder plugins to original position

--- plotbot/main.go
+++ plotbot/main.go
@@ -9,17 +9,18 @@ import (
 
 	_ "github.com/plotly/plotbot/web"
 
+
 	_ "github.com/plotly/plotbot/deployer"
 	_ "github.com/plotly/plotbot/funny"
-	_ "github.com/plotly/plotbot/healthy"
-	_ "github.com/plotly/plotbot/hooker"
-	_ "github.com/plotly/plotbot/mooder"
-	_ "github.com/plotly/plotbot/plotberry"
-	_ "github.com/plotly/plotbot/standup"
-	_ "github.com/plotly/plotbot/tabularasa"
 	_ "github.com/plotly/plotbot/totw"
-	_ "github.com/plotly/plotbot/webutils"
+	_ "github.com/plotly/plotbot/healthy"
+	_ "github.com/plotly/plotbot/mooder"
+	_ "github.com/plotly/plotbot/hooker"
+	_ "github.com/plotly/plotbot/standup"
+	_ "github.com/plotly/plotbot/plotberry"
+	_ "github.com/plotly/plotbot/tabularasa"
 	_ "github.com/plotly/plotbot/wicked"
+	_ "github.com/plotly/plotbot/webutils"
 )
 
 var configFile = flag.String("config", os.Getenv("HOME")+"/.plotbot", "config file")
