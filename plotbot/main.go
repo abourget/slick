a2e9f394c30d0ef6fed35df4a093dd0544ad86e1
Jody McIntyre <scjody@modernduck.com>
2014-12-09 11:07:41 -0500

    gofmt

--- plotbot/main.go
+++ plotbot/main.go
@@ -2,25 +2,24 @@ package main
 
 import (
 	"flag"
-	"os"
 	"github.com/plotly/plotbot"
+	"os"
 
 	_ "github.com/plotly/plotbot/rewarder"
 
 	_ "github.com/plotly/plotbot/web"
 
-
 	_ "github.com/plotly/plotbot/deployer"
 	_ "github.com/plotly/plotbot/funny"
-	_ "github.com/plotly/plotbot/totw"
 	_ "github.com/plotly/plotbot/healthy"
-	_ "github.com/plotly/plotbot/mooder"
 	_ "github.com/plotly/plotbot/hooker"
-	_ "github.com/plotly/plotbot/standup"
+	_ "github.com/plotly/plotbot/mooder"
 	_ "github.com/plotly/plotbot/plotberry"
+	_ "github.com/plotly/plotbot/standup"
 	_ "github.com/plotly/plotbot/tabularasa"
-	_ "github.com/plotly/plotbot/wicked"
+	_ "github.com/plotly/plotbot/totw"
 	_ "github.com/plotly/plotbot/webutils"
+	_ "github.com/plotly/plotbot/wicked"
 )
 
 var configFile = flag.String("config", os.Getenv("HOME")+"/.plotbot", "config file")
