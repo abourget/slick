2562df8d1d2a26ec17947ec7cd927bb2c640f7e7
BRONSOLO <chuck.james.bronson@gmail.com>
2015-02-19 12:10:07 -0500

    freeze the deps using godep

---
+++ Godeps/_workspace/src/github.com/jmcvetta/napping/util.go
@@ -0,0 +1,23 @@
+// Copyright (c) 2012-2013 Jason McVetta.  This is Free Software, released
+// under the terms of the GPL v3.  See http://www.gnu.org/copyleft/gpl.html for
+// details.  Resist intellectual serfdom - the ownership of ideas is akin to
+// slavery.
+
+package napping
+
+import (
+	"encoding/json"
+	"os"
+	"path/filepath"
+	"runtime"
+	"strconv"
+)
+
+func prettyPrint(v interface{}) {
+	_, file, line, _ := runtime.Caller(1)
+	lineNo := strconv.Itoa(line)
+	file = filepath.Base(file)
+	b, _ := json.MarshalIndent(v, "", "\t")
+	s := file + ":" + lineNo + ": \n" + string(b) + "\n"
+	os.Stderr.WriteString(s)
+}
