41c27aaaf36e3352f375e324cb50099f1fc21246
bpostlethwaite <post.ben.here@gmail.com>
2015-04-25 21:22:23 -0400

    github bugger

---
+++ bugger/bugger.go
@@ -0,0 +1,158 @@
+package bugger
+
+import (
+	"fmt"
+	"log"
+	"regexp"
+	"strconv"
+	"time"
+
+	"github.com/plotly/plotbot"
+	"github.com/plotly/plotbot/github"
+)
+
+func init() {
+	plotbot.RegisterPlugin(&Bugger{})
+}
+
+type Bugger struct {
+	bot      *plotbot.Bot
+	ghclient github.Client
+}
+
+type bugReport struct {
+	bugs []github.IssueItem
+}
+
+func (r *bugReport) addBug(issue github.IssueItem) {
+	r.bugs = append(r.bugs, issue)
+}
+
+func (r *bugReport) printReport(days int) (report string) {
+
+	dayheader := fmt.Sprintf(" BUG REPORT FOR LAST %d DAYS ", days) // 20 spaces
+	bar := "************************"
+
+	report = fmt.Sprintf("/quote " + bar + dayheader + bar + "\n")
+
+	report += fmt.Sprintf("|%-45s|%-7s|%-18s|\n", "bug title", "number", "squasher")
+	title := ""
+	for _, bug := range r.bugs {
+		if len(bug.Title) > 45 {
+			title = bug.Title[0:42] + "..."
+		} else {
+			title = bug.Title
+		}
+		report += fmt.Sprintf("|%-45s|%-7d|%-18s|\n", title, bug.Number, bug.LastClosedBy())
+	}
+
+	return
+}
+
+func (bugger *Bugger) getBugReport(days int) string {
+
+	repo := bugger.ghclient.Conf.Repos[0]
+
+	query := github.SearchQuery{
+		Repo:        repo,
+		Labels:      []string{"bug"},
+		ClosedSince: time.Now().Add(-time.Duration(days) * (24 * time.Hour)).Format("2006-01-02"),
+	}
+
+	issueList, err := bugger.ghclient.DoSearchQuery(query)
+	if err != nil {
+		log.Print(err)
+		return ""
+	}
+
+	/*
+	 * Get an array of issues matching Filters
+	 */
+	issueChan := make(chan github.IssueItem, 1)
+	go bugger.ghclient.DoEventQuery(issueList, issueChan)
+
+	reporter := bugReport{}
+
+	for issue := range issueChan {
+		reporter.addBug(issue)
+	}
+
+	return reporter.printReport(days)
+}
+
+func (bugger *Bugger) InitChatPlugin(bot *plotbot.Bot) {
+
+	/*
+	 * Get an array of issues matching Filters
+	 */
+	bugger.bot = bot
+
+	var conf struct {
+		Github github.Conf
+	}
+
+	bot.LoadConfig(&conf)
+
+	bugger.ghclient = github.Client{
+		Conf: conf.Github,
+	}
+
+	bot.ListenFor(&plotbot.Conversation{
+		HandlerFunc: bugger.ChatHandler,
+	})
+
+}
+
+func (bugger *Bugger) ChatHandler(conv *plotbot.Conversation, msg *plotbot.Message) {
+
+	if msg.MentionsMe && msg.Contains("bug report") {
+
+		re := regexp.MustCompile(".*last (\\d+)?\\s?(day|week).*")
+		hits := re.FindStringSubmatch(msg.Body)
+
+		var days, weeks int
+		var err error
+
+		if len(hits) == 3 {
+			howmany := hits[1]
+			dayOrWeek := hits[2]
+
+			if dayOrWeek == "day" {
+				if howmany == "" {
+					days = 7
+				} else {
+					days, err = strconv.Atoi(howmany)
+					if err != nil {
+						days = 7
+					}
+				}
+			} else {
+				if howmany == "" {
+					days = 7
+				} else {
+					weeks, err = strconv.Atoi(howmany)
+					if err != nil {
+						days = 7
+					} else {
+						days = 7 * weeks
+					}
+				}
+			}
+		} else {
+			days = 7
+		}
+
+		if days > 31 {
+			conv.Reply(msg, fmt.Sprintf("Whaoz, %d is too much data to compile - well maybe not, I am just scared", days))
+			return
+		}
+
+		conv.Reply(msg, fmt.Sprintf("hang on - let me ping those github kids"))
+
+		bugreport := bugger.getBugReport(days)
+
+		conv.Reply(msg, bugreport)
+	}
+	return
+
+}
---
+++ github/github.go
@@ -0,0 +1,150 @@
+package github
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strconv"
+	"time"
+)
+
+const searchIssueURL = "https://api.github.com/search/issues"
+
+type SearchQuery struct {
+	Repo        string
+	Labels      []string
+	ClosedSince string
+}
+
+type SearchResponse struct {
+	TotalCount int64 `json:"total_count"`
+	Items      []IssueItem
+}
+
+type GHUser struct {
+	Login string
+}
+
+type IssueItem struct {
+	Url       string
+	Title     string
+	Id        int
+	Number    int
+	Milestone string
+	Assignee  GHUser
+	State     string
+	Events    []IssueEvent
+}
+
+type IssueEvent struct {
+	Id    int
+	Actor GHUser
+	Event string
+}
+
+type Conf struct {
+	Authtoken  string
+	Repos      []string
+	GitHipName map[string]string
+}
+
+type Client struct {
+	Conf Conf
+}
+
+func (query *SearchQuery) Url() (url string) {
+
+	url = searchIssueURL
+
+	url += "?q="
+
+	if query.Repo != "" {
+		url += "+repo:" + query.Repo
+	}
+
+	if len(query.Labels) > 0 {
+		for _, value := range query.Labels {
+			url += "+label:" + value
+		}
+	}
+
+	if query.ClosedSince != "" {
+		url += "+closed:>" + query.ClosedSince
+	}
+
+	return
+}
+
+func (issue *IssueItem) LastClosedBy() string {
+	for i := len(issue.Events) - 1; i >= 0; i-- {
+		event := issue.Events[i]
+		if event.Event == "closed" {
+			return event.Actor.Login
+		}
+	}
+	return ""
+}
+
+func (ghclient *Client) Get(url string) (body []byte, err error) {
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return
+	}
+	req.SetBasicAuth(ghclient.Conf.Authtoken, "x-oauth-basic")
+
+	client := http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		return
+	}
+
+	body, err = ioutil.ReadAll(res.Body)
+
+	if err != nil {
+		return
+	}
+
+	res.Body.Close()
+
+	return
+}
+
+func (ghclient *Client) DoSearchQuery(query SearchQuery) ([]IssueItem, error) {
+
+	url := query.Url()
+	body, err := ghclient.Get(url)
+	if err != nil {
+		return nil, err
+	}
+
+	payload := SearchResponse{}
+	json.Unmarshal(body, &payload)
+
+	return payload.Items, nil
+}
+
+func (ghclient *Client) DoEventQuery(issueList []IssueItem, issueChan chan IssueItem) {
+
+	defer close(issueChan)
+
+	for _, issue := range issueList {
+
+		url := "https://api.github.com/repos/plotly/streambed/issues/" + strconv.Itoa(issue.Number) + "/events"
+		body, err := ghclient.Get(url)
+		if err != nil {
+			log.Print(err)
+		}
+
+		events := make([]IssueEvent, 0)
+		json.Unmarshal(body, &events)
+
+		issue.Events = events
+		issueChan <- issue
+
+		time.Sleep(500 * time.Millisecond)
+
+	}
+
+}
