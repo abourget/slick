db1002853517cd531b6d43d15b3889bd6e8a617d
Alexandre Bourget <alex@bourget.cc>
2015-10-09 17:05:09 -0400

    deploy_slick: added "can you" to politeness prefix
    announce to #slick-dev
    
    deploy_slick: announce when you've restarted, to the one who asked to deploy

--- db.go
+++ db.go
@@ -12,7 +12,7 @@ const slickDBDefaultBucket = "slick"
 // GetDBKey retrieves a `key` from persistent storage and JSON
 // unmarshales it into `v`.
 func (bot *Bot) GetDBKey(key string, v interface{}) error {
-	return bot.DB.View(func(tx *bolt.Tx) error {
+	return bot.DB.Update(func(tx *bolt.Tx) error {
 		bucket, err := tx.CreateBucketIfNotExists([]byte(slickDBDefaultBucket))
 		if err != nil {
 			return err
@@ -34,7 +34,7 @@ func (bot *Bot) GetDBKey(key string, v interface{}) error {
 
 // SetDBKey sets a
 func (bot *Bot) PutDBKey(key string, v interface{}) error {
-	return bot.DB.View(func(tx *bolt.Tx) error {
+	return bot.DB.Update(func(tx *bolt.Tx) error {
 		bucket, err := tx.CreateBucketIfNotExists([]byte(slickDBDefaultBucket))
 		if err != nil {
 			return err
@@ -45,9 +45,9 @@ func (bot *Bot) PutDBKey(key string, v interface{}) error {
 			return err
 		}
 
-		val := bucket.Put([]byte(key), jsonRes)
-		if val == nil {
-			return fmt.Errorf("not found")
+		err = bucket.Put([]byte(key), jsonRes)
+		if err != nil {
+			return err
 		}
 
 		return nil
