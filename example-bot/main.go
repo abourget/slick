a4c472c8599759891277b5855b81c35feeaef9ef
Alexandre Bourget <alex@bourget.cc>
2016-02-13 23:15:39 -0500

    Add the recognition plugin in the example.

--- example-bot/main.go
+++ example-bot/main.go
@@ -18,6 +18,7 @@ import (
 	_ "github.com/abourget/slick/webauth"
 	_ "github.com/abourget/slick/webutils"
 	_ "github.com/abourget/slick/wicked"
+	_ "github.com/abourget/slick/recognition"
 )
 
 var configFile = flag.String("config", os.Getenv("HOME")+"/.slick.conf", "config file")
