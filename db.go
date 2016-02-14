b2e768285267f55a7b9f280c1bbc6ed426940eff
Alexandre Bourget <alex@bourget.cc>
2016-02-13 23:21:20 -0500

    GetDBKey: no need for an Update transaction, View is fine.

--- db.go
+++ db.go
@@ -12,7 +12,7 @@ const slickDBDefaultBucket = "slick"
 // GetDBKey retrieves a `key` from persistent storage and JSON
 // unmarshales it into `v`.
 func (bot *Bot) GetDBKey(key string, v interface{}) error {
-	return bot.DB.Update(func(tx *bolt.Tx) error {
+	return bot.DB.View(func(tx *bolt.Tx) error {
 		bucket, err := tx.CreateBucketIfNotExists([]byte(slickDBDefaultBucket))
 		if err != nil {
 			return err
